12af33070aaf551d9f22c557b98c2542db708a61
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-06-15 14:18:46 +0300

    feat: add mongoose connection, create driver model and add route to create driver

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
-const app = express();
 const routes = require('./routes/routes');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE_URL);
+app.use(bodyParser.json());
 
 routes(app);
 module.exports = app;
--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -1,5 +1,10 @@
+const Driver = require('../models/driver');
 module.exports = {
 	greeting(req, res) {
 		res.send({ hi: 'there' });
+	},
+	create(req, res, next) {
+		const driverProps = req.body;
+		Driver.create(driverProps).then(driver => res.send(driver));
 	},
 };
